7938488695c786e031cfb9fc2fd6ce6dced37aaf
VulkanGO <max_egortsev@mail.ru>
2026-08-17 13:48:21 +0300

    a

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import *
 from sqlalchemy.orm import *
 from sqlalchemy.ext.asyncio import *
 
-DATABASE_URL = "postgresql+asyncpg://weather_user:123456789@localhost:5432/weather_db"
+DATABASE_URL = "postgresql://postgres:xohm08r8TbtbA647@db.cuoyymbzzlobknuxfaof.supabase.co:5432/postgres"
 
 async_engine = create_async_engine(DATABASE_URL, echo=True)
 async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
