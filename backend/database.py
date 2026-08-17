f3750eb5bbb1c0ad267e91f9f9188d0a3023df37
VulkanGO <max_egortsev@mail.ru>
2026-08-17 18:46:39 +0300

    Fix database URL for asyncpg (import from config)

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import *
 from sqlalchemy.orm import *
 from sqlalchemy.ext.asyncio import *
-
-DATABASE_URL = "postgresql://postgres:xohm08r8TbtbA647@db.cuoyymbzzlobknuxfaof.supabase.co:5432/postgres"
+from .config import DATABASE_URL
 
 async_engine = create_async_engine(DATABASE_URL, echo=True)
 async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
