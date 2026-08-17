53d7f43b7288ae651f5e6473cbd368693787b218
VulkanGO <max_egortsev@mail.ru>
2026-08-17 18:35:27 +0300

    main.py

--- backend/main.py
+++ backend/main.py
@@ -4,15 +4,10 @@ from .routers import users_router, cities_router
 
 app = FastAPI(title="FastAPI Погода", version="0.1.0")
 
-origins = [
-    "https://weather-app-mu-nine-21.vercel.app",
-    "http://localhost:5173",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
