5bc4c00d5f0f0f59eaca706c68acd94ba0570763
VulkanGO <max_egortsev@mail.ru>
2026-08-18 17:26:32 +0300

    Fix: 404 error and logout of enter web

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
+import { onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { useAuthStore} from "./stores/auth.js"
 
 const router = useRouter()
 const authStore = useAuthStore()
+
+OnMounted(async () => {
+    await authStore.checkAuth()
+})
 
 const handleLogout = () => {
     if (confirm("Вы уверены, что хотите выйти?"))
